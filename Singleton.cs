45533745d863eb400cd7b7767148a4d239cb7e00
Ray Joseph De Castro <zengineer@me.com>
2013-06-03 12:46:24 -0600

    Fixed minor alignment/spacing issues

--- Singleton.cs
+++ Singleton.cs
@@ -8,25 +8,26 @@ public class Singleton
 {
     private static Singleton instance;
 
-	private Singleton()
-	{
+    private Singleton()
+    {
 
-	}
+    }
 
-	public static Singleton Instance
-	{
+    public static Singleton Instance
+    {
         get
-		{
+        {
             if (instance == null)
-			{
-				instance = new Singleton();
-			}
-			return instance;
-		}
-	}
+            {
+                instance = new Singleton();
+            }
+            
+            return instance;
+        }
+    }
 
-	public void MethodA()
-	{
-		Console.WriteLine("Singleton Method call");
-	}
+    public void MethodA()
+    {
+        Console.WriteLine("Singleton Method call");
+    }
 }
